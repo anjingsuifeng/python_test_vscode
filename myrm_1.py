8a430cdf3461a93bca54ef99d985e9463b4eeb8c
lili <804645205@qq.com>
2017-03-23 23:47:34 +0800

    night

--- myrm_1.py
+++ myrm_1.py
@@ -1,24 +1,32 @@
-#!?usr/bin/python2
+#!/usr/bin/python2
 #coding:utf-8
-import os
+import os, shutil
 def myrm(path):
     if os.path.isfile(path):
         os.remove(path)
     else:    
         file_list = os.listdir(path)
         print file_list
-        if not file_list:
-            os.rmdir(path)
+        digui_deldir(path)
+        # if s == True:
+        #     return
+        # else:
+        #     digui_deldir(s)
+ALL_DEL = "DEL"
+def digui_deldir(path):
+    for file_name in os.listdir(path):
+        file_path = path + '/' + file_name
+        if os.path.isfile(file_path):
+            print("ll--file")
+            os.remove(file_path)
         else:
-            # print "%s incloud dirs,you can not del it" %path
-            for i in range(len(file_list)):
-                file_path = path + '/' + file_list[i]
-                print file_path
-                os.remove(file_path)
-                file_path = ''
-            os.rmdir(path)
-
-
+            print("ll--dir")
+            confirm = raw_input("File: %s Input DEL" % file_path)
+            if not (confirm == ALL_DEL):
+                continue
+            else:
+                shutil.rmtree(file_path)
+            
 if __name__ == "__main__":
-    input_path = raw_input("please input a path\n(the current dir use './')")
-    myrm(input_path)
+    path = raw_input("please input a path\n(the current dir use './')")
+    digui_deldir(path)
